7c1fce48461e8e2bbed97b0b206066452a9e933e
jhb0430 <leky2224@gmail.com>
2024-11-29 17:28:27 +0900

    [배지현] - memo 프로젝트 생성

---
+++ src/test/java/com/jhb0430/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhb0430.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
